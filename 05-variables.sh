71f79112df83b94d49e6698994f6fb53857d133d
Chiranjeevi <etichiranjeevi125@gmail.com>
2024-02-06 14:30:52 +0530

    added 2nd script cmd  read_s_variable1

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,8 @@ echo "Please enter your username"
 read -s  USERNAME #the value entered above will be attached to USERNAME variable
 
 echo "Username is: $USERNAME"
+
+echo "Please enter your Password:"
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is :$PASSWORD"
