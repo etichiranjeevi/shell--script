6a773e7252c4d12cb7f1e6f0d20a830bf625218f
Chiranjeevi <etichiranjeevi125@gmail.com>
2024-02-06 14:25:35 +0530

    added 2nd script cmd  read-variable1

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "Please enter your username"
 
-read USERNAME #the value entered above will be attached to USERNAME variable
+read -s  USERNAME #the value entered above will be attached to USERNAME variable
 
 echo "Username is: $USERNAME"
